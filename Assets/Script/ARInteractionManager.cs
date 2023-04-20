147c59976d10b3f0a513a01720ce9c7ef6315bf0
figonzal1 <felipe.gonzalezalarcon94@gmail.com>
2023-04-20 17:10:05 -0400

    rotate objects

--- Assets/Script/ARInteractionManager.cs
+++ Assets/Script/ARInteractionManager.cs
@@ -14,6 +14,7 @@ public class ARInteractionManager : MonoBehaviour
 
 	private GameObject arPointer;
 	private GameObject item3DModel;
+	private GameObject itemSelected;
 	public GameObject Item3DModel
 	{
 		set
@@ -26,6 +27,9 @@ public class ARInteractionManager : MonoBehaviour
 	}
 	private bool isInitialPosition;
 	private bool isOverUI;
+
+	private Vector2 initialTouchPos;
+	private bool isOver3DModel;
 
 	// Start is called before the first frame update
 	void Start()
@@ -85,6 +89,7 @@ public class ARInteractionManager : MonoBehaviour
 			{
 				var touchPosition = touchOne.position;
 				isOverUI = isTapOverUI(touchPosition);
+				isOver3DModel = isTapOver3DModel(touchPosition);
 			}
 
 
@@ -94,12 +99,58 @@ public class ARInteractionManager : MonoBehaviour
 				{
 					Pose hitPose = hits[0].pose;
 
-					if(!isOverUI){
+					if (!isOverUI && isOver3DModel)
+					{
 						transform.position = hitPose.position;
 					}
 				}
 			}
+
+			//Rotar modelos 3d
+			if (Input.touchCount == 2)
+			{
+				Touch touchTwo = Input.GetTouch(1);
+
+				if (touchOne.phase == TouchPhase.Began || touchTwo.phase == TouchPhase.Began)
+				{
+					initialTouchPos = touchTwo.position - touchOne.position;
+				}
+
+				if (touchOne.phase == TouchPhase.Moved || touchTwo.phase == TouchPhase.Moved)
+				{
+					Vector2 currentTouchPos = touchTwo.position - touchOne.position;
+					float angle = Vector2.SignedAngle(initialTouchPos, currentTouchPos);
+					item3DModel.transform.rotation = Quaternion.Euler(0, item3DModel.transform.eulerAngles.y - angle, 0);
+					initialTouchPos = currentTouchPos;
+				}
+			}
+
+
+			if(isOver3DModel && item3DModel == null  && !isOverUI) {
+				GameManager.instance.ARPosition();
+				item3DModel = itemSelected;
+				itemSelected = null;
+				arPointer.SetActive(true);
+				transform.position = item3DModel.transform.position;
+
+				item3DModel.transform.parent = arPointer.transform;
+			}
 		}
+	}
+
+	private bool isTapOver3DModel(Vector2 touchPosition)
+	{
+		Ray ray = arCamera.ScreenPointToRay(touchPosition);
+
+		if(Physics.Raycast(ray,out RaycastHit hit3DModel)){
+
+			if(hit3DModel.collider.CompareTag("Item")) {
+
+				itemSelected = hit3DModel.transform.gameObject;
+				return true;
+			}
+		}
+		return false;
 	}
 
 	private bool isTapOverUI(Vector2 touchPosition)
